b817a5b353ce2caf29f691190352d644ffe17daa
Aji Fatur <ajifatur32@gmail.com>
2022-02-04 18:40:13 +0700

    v1.4.1

--- main.php
+++ main.php
@@ -3,7 +3,7 @@
 Plugin Name: Spandiv
 Plugin URI: https://spandiv.xyz/
 Description: Custom Wordpress Features.
-Version: 1.4
+Version: 1.4.1
 Author: Spandiv
 Author URI: https://spandiv.xyz/
 License: GPL2
@@ -43,7 +43,8 @@ function addAdminPageContent() {
 
 require 'plugin-update/plugin-update-checker.php';
 $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
-  'https://github.com/praseetyaa/spandiv-press',
+  // 'https://github.com/praseetyaa/spandiv-press',
+  site_url('wp-content/plugins/spandiv/plugin-update/examples/plugin.json'),
   __FILE__, //Full path to the main plugin file or functions.php.
   'spandiv'
 );
