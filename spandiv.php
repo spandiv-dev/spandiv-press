669d30abafe1cf8ed1b60b1822095a6668304059
Isna Prasetyo <praseetyaa@gmail.com>
2022-05-28 23:03:20 +0700

    remove price

--- spandiv.php
+++ spandiv.php
@@ -4,7 +4,7 @@
 Plugin Name: Spandiv
 Plugin URI: https://spandiv.xyz/
 Description: Custom WordPress Features.
-Version: 1.5.6
+Version: 1.5.7
 Author: Spandiv
 Author URI: https://spandiv.xyz/
 License: GPL2
